a737a7ac223df5c413cef3b074313bda1ca10dd9
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-04-27 23:30:36 +0200

    pfs: use SDL's file handling functions

--- src/pfs.c
+++ src/pfs.c
@@ -1,4 +1,4 @@
-/** @file pfs.c
+/** @File pfs.c
  *
  *  Packed file system implementation.
  *  Adapted version, originally from The Mistral Report:
@@ -19,10 +19,10 @@
 
 char mDataPath[kDataPath_MaxLength];
 
-void    init_file_reader(const char * dataFilePath);
-size_t  size_of_file(const char * path);
-Uint8  *load_binary_file_from_path(const char * path);
-FILE   *open_binary_file_from_path(const char * path);
+void       init_file_reader(const char * dataFilePath);
+size_t     size_of_file(const char * path);
+Uint8     *load_binary_file_from_path(const char * path);
+SDL_RWops *open_binary_file_from_path(const char * path);
 
 void init_file_reader(const char * dataFilePath)
 {
@@ -31,33 +31,33 @@ void init_file_reader(const char * dataFilePath)
 
 size_t size_of_file(const char * path)
 {
-    FILE   *mDataPack = fopen(mDataPath, "rb");
-    char    buffer[85];
-    int     c;
-    Uint32  size      = 0;
-    Uint32  offset    = 0;
-    Uint16  entries   = 0;
+    SDL_RWops *mDataPack = SDL_RWFromFile(mDataPath, "rb");
+    char       buffer[85];
+    int        c;
+    Uint32     size      = 0;
+    Uint32     offset    = 0;
+    Uint16     entries   = 0;
 
-    if (1 != fread(&entries, 2, 1, mDataPack))
+    if (1 != SDL_RWread(mDataPack, &entries, 2, 1))
     {
         /* Nothing to do here. */
     }
 
     for (c = 0; c < entries; ++c)
     {
-        uint8_t stringSize = 0;
+        Uint8 stringSize = 0;
 
-        if (1 != fread(&offset, 4, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &offset, 4, 1))
         {
             /* Nothing to do here. */
         }
 
-        if (1 != fread(&stringSize, 1, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &stringSize, 1, 1))
         {
             /* Nothing to do here. */
         }
 
-        if (1 != fread(&buffer, stringSize + 1, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &buffer, stringSize + 1, 1))
         {
             /* Nothing to do here. */
         }
@@ -75,29 +75,29 @@ found:
         exit(-1);
     }
 
-    fseek(mDataPack, offset, SEEK_SET);
+    SDL_RWseek(mDataPack, offset, RW_SEEK_SET);
 
-    if (1 != fread(&size, 4, 1, mDataPack))
+    if (1 != SDL_RWread(mDataPack, &size, 4, 1))
     {
         /* Nothing to do here. */
     }
 
-    fclose(mDataPack);
+    SDL_RWclose(mDataPack);
 
     return size;
 }
 
 Uint8 *load_binary_file_from_path(const char * path)
 {
-    FILE   *mDataPack = fopen(mDataPath, "rb");
-    Uint32  offset    = 0;
-    Uint16  entries   = 0;
-    char    buffer[85];
-    int     c;
-    Uint32  size      = 0;
-    Uint8  *toReturn;
+    SDL_RWops *mDataPack = SDL_RWFromFile(mDataPath, "rb");
+    Uint32     offset    = 0;
+    Uint16     entries   = 0;
+    char       buffer[85];
+    int        c;
+    Uint32     size      = 0;
+    Uint8     *toReturn;
 
-    if (1 != fread(&entries, 2, 1, mDataPack))
+    if (1 != SDL_RWread(mDataPack, &entries, 2, 1))
     {
         /* Nothing to do here. */
     }
@@ -106,17 +106,17 @@ Uint8 *load_binary_file_from_path(const char * path)
     {
         Uint8 stringSize = 0;
 
-        if (1 != fread(&offset, 4, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &offset, 4, 1))
         {
             /* Nothing to do here. */
         }
 
-        if (1 != fread(&stringSize, 1, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &stringSize, 1, 1))
         {
             /* Nothing to do here. */
         }
 
-        if (1 != fread(&buffer, stringSize + 1, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &buffer, stringSize + 1, 1))
         {
             /* Nothing to do here. */
         }
@@ -134,35 +134,35 @@ found:
         exit(-1);
     }
 
-    fseek(mDataPack, offset, SEEK_SET);
+    SDL_RWseek(mDataPack, offset, RW_SEEK_SET);
 
-    if (1 != fread(&size, 4, 1, mDataPack))
+    if (1 != SDL_RWread(mDataPack, &size, 4, 1))
     {
         /* Nothing to do here. */
     }
 
     toReturn = (Uint8 *) malloc(size);
 
-    if (size != fread(toReturn, sizeof(uint8_t), size, mDataPack))
+    if (size != SDL_RWread(mDataPack, toReturn, sizeof(Uint8), size))
     {
         /* Nothing to do here. */
     }
 
-    fclose(mDataPack);
+    SDL_RWclose(mDataPack);
 
     return toReturn;
 }
 
-FILE *open_binary_file_from_path(const char * path)
+SDL_RWops *open_binary_file_from_path(const char * path)
 {
-    FILE   *mDataPack = fopen(mDataPath, "rb");
-    Uint32  offset    = 0;
-    Uint16  entries   = 0;
-    char    buffer[85];
-    int     c;
-    Uint32  size      = 0;
+    SDL_RWops *mDataPack = SDL_RWFromFile(mDataPath, "rb");
+    Uint32     offset    = 0;
+    Uint16     entries   = 0;
+    char       buffer[85];
+    int        c;
+    Uint32     size      = 0;
 
-    if (1 != fread(&entries, 2, 1, mDataPack))
+    if (1 != SDL_RWread(mDataPack, &entries, 2, 1))
     {
         /* Nothing to do here. */
     }
@@ -171,17 +171,17 @@ FILE *open_binary_file_from_path(const char * path)
     {
         Uint8 stringSize = 0;
 
-        if (1 != fread(&offset, 4, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &offset, 4, 1))
         {
             /* Nothing to do here. */
         }
 
-        if (1 != fread(&stringSize, 1, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &stringSize, 1, 1))
         {
             /* Nothing to do here. */
         }
 
-        if (1 != fread(&buffer, stringSize + 1, 1, mDataPack))
+        if (1 != SDL_RWread(mDataPack, &buffer, stringSize + 1, 1))
         {
             /* Nothing to do here. */
         }
@@ -201,8 +201,9 @@ found:
         exit(-1);
     }
 
-    fseek(mDataPack, offset, SEEK_SET);
-    if (1 != fread(&size, 4, 1, mDataPack))
+    SDL_RWseek(mDataPack, offset, RW_SEEK_SET);
+
+    if (1 != SDL_RWread(mDataPack, &size, 4, 1))
     {
         /* Nothing to do here. */
     }
